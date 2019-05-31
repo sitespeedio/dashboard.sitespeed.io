eaf456b7d0ab158c60e16802ed2a016b935aab89
soulgalore <peter@soulgalore.com>
2019-05-31 23:47:00 +0200

    cleanup and test with ff

--- run.sh
+++ run.sh
@@ -38,44 +38,48 @@ function control() {
 # Instead use the latest tagged version as the next row
 # DOCKER_CONTAINER=sitespeedio/sitespeed.io:9.2.0
 DOCKER_CONTAINER=sitespeedio/sitespeed.io-autobuild:latest
-DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /result:/result -v /etc/localtime:/etc/localtime:ro "
+DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro "
 CONFIG="--config /sitespeed.io/config"
 sudo modprobe ifb numifbs=1
+BROWSERS=(chrome firefox)
 
 while true
 do
     for urls in $SERVER/desktop/urls/* ; do
-        NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
-        docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/desktop.json $urls
-        control
+      for browser in "${BROWSERS[@]}"
+        do
+          NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
+          docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/desktop.json -b $browser $urls
+          control
+        done
     done
 
     for urls in $SERVER/desktop/scripts/* ; do
         NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
-        docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/desktop.json --multi --spa $urls
+        docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/desktop.json --multi --spa $urls
         control
     done
     for urls in $SERVER/mobile/urls/* ; do
         NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
-        docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/mobile.json $urls
+        docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/mobile.json $urls
         control
     done
 
     for urls in $SERVER/mobile/scripts/* ; do
         NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
-        docker run $CABLE $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/mobile.json --multi --spa $urls
+        docker run $DOCKER_SETUP $DOCKER_CONTAINER $NAMESPACE $CONFIG/mobile.json --multi --spa $urls
         control
     done
 
     for urls in $SERVER/replay/urls/* ; do
         NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
-        docker run $CABLE $DOCKER_SETUP -e REPLAY=true -e LATENCY=100 $DOCKER_CONTAINER $NAMESPACE $CONFIG/replay.json $urls
+        docker run $DOCKER_SETUP -e REPLAY=true -e LATENCY=100 $DOCKER_CONTAINER $NAMESPACE $CONFIG/replay.json $urls
         control
     done
 
     for urls in $SERVER/webpagetest/urls/* ; do
         NAMESPACE="--graphite.namespace sitespeed_io.$(basename ${urls%.*})"
-        docker run $CABLE $DOCKER_SETUP -e REPLAY=true -e LATENCY=100 $DOCKER_CONTAINER $NAMESPACE $CONFIG/webpagetest.json $urls
+        docker run $DOCKER_SETUP -e REPLAY=true -e LATENCY=100 $DOCKER_CONTAINER $NAMESPACE $CONFIG/webpagetest.json $urls
         control
     done
 
