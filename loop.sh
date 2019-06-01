2052ebea733135c19a5d9b42b7ec479de9d36a0b
soulgalore <peter@soulgalore.com>
2019-06-01 13:05:59 +0200

    log git

--- loop.sh
+++ loop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+LOGFILE=/tmp/sitespeed.io.log
+exec > $LOGFILE 2>&1
 
 SERVER=$1
 while true
 do
+    ## For each iteration, we pull the latest code from git and run
     git pull
     source run.sh $SERVER
     result=$?
