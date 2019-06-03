363cce2636aac964b8576ee7d42ead07f8c4d17e
Peter Hedenskog <phedenskog@wikimedia.org>
2019-06-03 16:33:57 +0200

    Increase the max limit memory for NodeJS (#2)

--- run.sh
+++ run.sh
@@ -41,7 +41,7 @@ function control() {
 # DOCKER_CONTAINER=sitespeedio/sitespeed.io:9.2.1
 
 DOCKER_CONTAINER=sitespeedio/sitespeed.io-autobuild:latest
-DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro "
+DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
 CONFIG="--config /sitespeed.io/config"
 BROWSERS=(chrome firefox)
 
