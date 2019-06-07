95ea256154e23319a5972ebcd0909fe3ab5c02ad
Peter Hedenskog <phedenskog@wikimedia.org>
2019-06-07 13:59:44 +0100

    Move control and cleanup to the loop, making the user flow easier (#5)

--- run.sh
+++ run.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
-LOGFILE=/tmp/sitespeed.io.log
-exec > $LOGFILE 2>&1
-CONTROL_FILE="./sitespeed.run"
-SERVER=$1
-
-# The first parameter is the server name, so we can find the right tests to run
-if [ -z "$1" ] 
-then
-    echo "Missing server input! You need to run with a parameter that gives the path to the configuration "
-    exit 1
-fi
-
-# In your curent dir we will place a file called sitespeed.run that shows that the tests are running
-# If you want to stop the tests gracefully, remove that file: rm sitespeed.io and wait for 
-# the tests to finish (tail -f /tmp/sitespeed.io)
-
-# You cannot start multiple instances!
-if [ -f "$CONTROL_FILE" ]
-then
-  echo "$CONTROL_FILE exist, do you have running tests?"
-  exit 1;
-else
-  touch $CONTROL_FILE
-fi
-
-function control() {
-  if [ -f "$CONTROL_FILE" ]
-  then
-    echo "$CONTROL_FILE found. Make another run ..."
-  else
-    echo "$CONTROL_FILE not found - stopping after cleaning up ..."
-    docker system prune --all --volumes -f
-    echo "Exit"
-    exit 1
-  fi
-}
 
 # We use the autobuild to always test our new functionality. But YOU should not do that!
 # Instead use the latest tagged version as the next row
@@ -44,9 +8,6 @@ DOCKER_CONTAINER=sitespeedio/sitespeed.io-autobuild:latest
 DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
 CONFIG="--config /sitespeed.io/config"
 BROWSERS=(chrome firefox)
-
-# To get thottle to work (https://github.com/sitespeedio/throttle)!
-sudo modprobe ifb numifbs=1
 
 # We loop through all directories we have
 # We run many tests to verify the functionality of sitespeed.io and you can simplify this by
@@ -113,5 +74,3 @@ done
 # If you run a stable version (as YOU should), you don't need to remove the container
 docker system prune --all --volumes -f
 sleep 20
-rm $CONTROL_FILE
-
