675f9d8723aae4de6f79a79293f83a48d1ffc0cf
Peter Hedenskog <peter@soulgalore.com>
2019-07-05 11:11:41 +0200

    lint on Travis (#7)
    * lint on Travis

--- nyc3-1/desktop/scripts/loginWikipedia.js
+++ nyc3-1/desktop/scripts/loginWikipedia.js
@@ -1,7 +1,7 @@
 module.exports = async function(context, commands) {
   // We start by navigating to the login page.
   await commands.navigate(
-    "https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page"
+    'https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page'
   );
 
   // When we fill in a input field/click on a link we wanna
@@ -13,23 +13,23 @@ module.exports = async function(context, commands) {
     const userName = context.options.wikipedia.user;
     const password = context.options.wikipedia.password;
 
-    await commands.addText.byId(userName, "wpName1");
-    await commands.addText.byId(password, "wpPassword1");
+    await commands.addText.byId(userName, 'wpName1');
+    await commands.addText.byId(password, 'wpPassword1');
 
     // Start the measurement before we click on the
     // submit button. Sitespeed.io will start the video recording
     // and prepare everything.
-    await commands.measure.start("login");
+    await commands.measure.start('login');
     // Find the sumbit button and click it and then wait
     // for the pageCompleteCheck to finish
-    await commands.click.byIdAndWait("wpLoginAttempt");
+    await commands.click.byIdAndWait('wpLoginAttempt');
     // Stop and collect the measurement before the next page we want to measure
     await commands.measure.stop();
     // Measure the Barack Obama page as a logged in user
-    await commands.measure.start("https://en.wikipedia.org/wiki/Barack_Obama");
+    await commands.measure.start('https://en.wikipedia.org/wiki/Barack_Obama');
     // And then measure the president page
     return commands.measure.start(
-      "https://en.wikipedia.org/wiki/President_of_the_United_States"
+      'https://en.wikipedia.org/wiki/President_of_the_United_States'
     );
   } catch (e) {
     context.log.error(e);
--- nyc3-1/desktop/scripts/spa.js
+++ nyc3-1/desktop/scripts/spa.js
@@ -1,15 +1,15 @@
 module.exports = async function(context, commands) {
-  commands.meta.setTitle("Test Grafana SPA");
+  commands.meta.setTitle('Test Grafana SPA');
   commands.meta.setDescription(
-    "Test the first page, click the timepicker and then choose <b>Last 30 days</b> and measure that page."
+    'Test the first page, click the timepicker and then choose <b>Last 30 days</b> and measure that page.'
   );
   await commands.measure.start(
-    "https://dashboard.sitespeed.io/d/000000044/page-timing-metrics?orgId=1",
-    "pageTimingMetricsDefault"
+    'https://dashboard.sitespeed.io/d/000000044/page-timing-metrics?orgId=1',
+    'pageTimingMetricsDefault'
   );
-  await commands.click.byClassName("gf-timepicker-nav-btn");
+  await commands.click.byClassName('gf-timepicker-nav-btn');
   await commands.wait.byTime(1000);
-  await commands.measure.start("pageTimingMetrics30Days");
-  await commands.click.byLinkTextAndWait("Last 30 days");
+  await commands.measure.start('pageTimingMetrics30Days');
+  await commands.click.byLinkTextAndWait('Last 30 days');
   await commands.measure.stop();
 };
