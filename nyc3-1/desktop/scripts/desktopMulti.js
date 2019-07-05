5b97ee78d090fb803f245f1609f4a34fb4dd0391
Peter Hedenskog <phedenskog@wikimedia.org>
2019-07-05 11:08:04 +0200

    lint on Travis

--- nyc3-1/desktop/scripts/desktopMulti.js
+++ nyc3-1/desktop/scripts/desktopMulti.js
@@ -1,7 +1,7 @@
 module.exports = async function(context, commands) {
-  await commands.measure.start("https://en.wikipedia.org/wiki/Main_Page");
-  await commands.measure.start("https://en.wikipedia.org/wiki/Barack_Obama");
+  await commands.measure.start('https://en.wikipedia.org/wiki/Main_Page');
+  await commands.measure.start('https://en.wikipedia.org/wiki/Barack_Obama');
   return commands.measure.start(
-    "https://en.wikipedia.org/wiki/Democratic_Party_(United_States)"
+    'https://en.wikipedia.org/wiki/Democratic_Party_(United_States)'
   );
 };
