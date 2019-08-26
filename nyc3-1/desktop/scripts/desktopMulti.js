15a6e77a10300a76257dc4f41d347b650dfe7e3e
soulgalore <peter@soulgalore.com>
2019-08-26 23:47:19 +0200

    make it white for better visual metrics

--- nyc3-1/desktop/scripts/desktopMulti.js
+++ nyc3-1/desktop/scripts/desktopMulti.js
@@ -1,6 +1,12 @@
 module.exports = async function(context, commands) {
   await commands.measure.start('https://en.wikipedia.org/wiki/Main_Page');
+  await commands.js.run(
+    'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";'
+  );
   await commands.measure.start('https://en.wikipedia.org/wiki/Barack_Obama');
+  await commands.js.run(
+    'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";'
+  );
   return commands.measure.start(
     'https://en.wikipedia.org/wiki/Democratic_Party_(United_States)'
   );
