c235611a4235323f45c722cf56cb163c125bec45
soulgalore <peter@soulgalore.com>
2020-01-01 16:10:01 +0100

    Add some reading time

--- tests/nyc3-1/desktop/scripts/desktopMulti.js
+++ tests/nyc3-1/desktop/scripts/desktopMulti.js
@@ -1,9 +1,17 @@
 module.exports = async function(context, commands) {
+  commands.meta.setTitle('Test visiting multiple pages');
+  commands.meta.setDescription(
+    'First hit the Main_Page with an empty browser cache and then visit Barack, followed buy the Democratic Party'
+  );
   await commands.measure.start('https://en.wikipedia.org/wiki/Main_Page');
+  // Fake some read time
+  await commands.wait.byTime(21000);
   await commands.js.run(
     'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";'
   );
   await commands.measure.start('https://en.wikipedia.org/wiki/Barack_Obama');
+  // Fake some read time
+  await commands.wait.byTime(21000);
   await commands.js.run(
     'document.body.innerHTML = ""; document.body.style.backgroundColor = "white";'
   );
