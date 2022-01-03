d6e36fbd8a64579e0f6da796932333bb0fc4a324
soulgalore <peter@soulgalore.com>
2022-01-03 18:53:47 +0100

    new Grafana, new layout

--- tests/desktop/spa.js
+++ tests/desktop/spa.js
@@ -1,4 +1,4 @@
-module.exports = async function(context, commands) {
+module.exports = async function (context, commands) {
   commands.meta.setTitle('Test Grafana SPA');
   commands.meta.setDescription(
     'Test the first page, click the timepicker and then choose <b>Last 30 days</b> and measure that page.'
@@ -7,12 +7,12 @@ module.exports = async function(context, commands) {
     'https://dashboard.sitespeed.io/d/000000064/drilldown?orgId=1',
     'pageTimingMetricsDefault'
   );
-  await commands.click.byClassName('toolbar-button css-bdxldk-toolbar-button');
+  await commands.click.byClassName('toolbar-button css-1hzht9j-toolbar-button');
   await commands.wait.byTime(3000);
   await commands.measure.start('pageTimingMetrics30Days');
   try {
     await commands.click.byXpathAndWait(
-      '/html/body/div/div/main/div[3]/header/div/div[4]/div/div[1]/div/div/div/div[1]/div[1]/section[1]/fieldset/ul/li[11]'
+      '/html/body/div/div/main/div[3]/header/div/div[4]/div/div[1]/section/div/div[1]/div[1]/div[2]/div[1]/ul/li[11]/label'
     );
     await commands.wait.byTime(5000);
   } catch (e) {
