69354c0f924007b29e07ed3099e4a28e8349edff
soulgalore <peter@soulgalore.com>
2022-09-10 18:18:15 +0200

    new updated Grafana

--- tests/docker/desktop/spa.js
+++ tests/docker/desktop/spa.js
@@ -7,12 +7,12 @@ module.exports = async function (context, commands) {
     'https://dashboard.sitespeed.io/d/9NDMzFfMk/page-metrics-desktop?orgId=1&var-base=sitespeed_io&var-path=desktop&var-testname=spa&var-group=dashboard_sitespeed_io&var-page=pageTimingMetricsDefault&var-browser=chrome&var-connectivity=cable&var-function=median&var-resulturl=https:%2F%2Fdata.sitespeed.io%2F&var-screenshottype=jpg',
     'pageTimingMetricsDefault'
   );
-  await commands.click.byClassName('toolbar-button css-qo3whe-toolbar-button');
+  await commands.click.byClassName('toolbar-button css-1umxd5y-toolbar-button');
   await commands.wait.byTime(3000);
   await commands.measure.start('pageTimingMetrics30Days');
   try {
     await commands.click.byXpathAndWait(
-      '/html/body/div[1]/div[1]/main/div[3]/header/nav/div[2]/div/div[1]/section/div/div[1]/div[1]/div[2]/div[1]/ul/li[11]'
+      '//*[@id="TimePickerContent"]/div[1]/div[1]/div[2]/div[1]/ul/li[11]'
     );
     await commands.wait.byTime(5000);
   } catch (e) {
