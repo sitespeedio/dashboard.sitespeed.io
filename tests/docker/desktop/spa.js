08d6620063f3fe630d7a11d07d6ad8c2c125cc47
soulgalore <peter@soulgalore.com>
2022-10-13 13:19:24 +0200

    new Grafana

--- tests/docker/desktop/spa.js
+++ tests/docker/desktop/spa.js
@@ -7,7 +7,7 @@ module.exports = async function (context, commands) {
     'https://dashboard.sitespeed.io/d/9NDMzFfMk/page-metrics-desktop?orgId=1&var-base=sitespeed_io&var-path=desktop&var-testname=spa&var-group=dashboard_sitespeed_io&var-page=pageTimingMetricsDefault&var-browser=chrome&var-connectivity=cable&var-function=median&var-resulturl=https:%2F%2Fdata.sitespeed.io%2F&var-screenshottype=jpg',
     'pageTimingMetricsDefault'
   );
-  await commands.click.byClassName('toolbar-button css-1umxd5y-toolbar-button');
+  await commands.click.byClassName('css-1umxd5y-toolbar-button');
   await commands.wait.byTime(3000);
   await commands.measure.start('pageTimingMetrics30Days');
   try {
