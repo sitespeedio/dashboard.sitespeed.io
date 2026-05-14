cf4d620f471efcfb837a71ed1d4a1cca9f9829ee
Peter Hedenskog <phedenskog@wikimedia.org>
2026-05-14 11:55:04 +0200

    smarter waits

--- tests/docker/desktop/spa.js
+++ tests/docker/desktop/spa.js
@@ -11,8 +11,13 @@ export default async function(context, commands) {
     'https://dashboard.sitespeed.io/d/9NDMzFfMk/page-metrics-desktop?orgId=1&var-base=sitespeed_io&var-path=desktop&var-testname=spa&var-group=dashboard_sitespeed_io&var-page=pageTimingMetricsDefault&var-browser=chrome&var-connectivity=cable&var-function=median&var-resulturl=https:%2F%2Fdata.sitespeed.io%2F&var-screenshottype=jpg',
     'pageTimingMetricsDefault'
   );
-  await commands.click.byXpath('//button[@data-testid="data-testid TimePicker Open Button"]');
-  await commands.wait.byTime(3000);
+  await commands.click.byXpath(
+    '//button[@data-testid="data-testid TimePicker Open Button"]'
+  );
+  await commands.wait.bySelector(
+    '[data-testid="data-testid TimePicker Overlay Content"]',
+    10000
+  );
   await commands.measure.start('pageTimingMetrics30Days');
   try {
     await commands.click.byXpath('//label[normalize-space(.)="Last 30 days"]');
